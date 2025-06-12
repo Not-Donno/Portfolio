8b9a69fc4895fc2f1ef060ea69eafe095471022d
Donno//Abhinav <abhinavgautam123456@gmail.com>
2025-06-12 15:16:27 +0545

    hero

--- app/hero-section/Hero.tsx
+++ app/hero-section/Hero.tsx
@@ -106,7 +106,7 @@ const Hero = () => {
           variants={bodyAnimation}
         >
           <p className="z-50 text-center text-[16px] font-medium text-[#e4ded7] md:text-[20px] lg:text-left">
-            Web Developer
+            Java Developer/Web Developer/Graphic Designer
           </p>
         </motion.div>
 
